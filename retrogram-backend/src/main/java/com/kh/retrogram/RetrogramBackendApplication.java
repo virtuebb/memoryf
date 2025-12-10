9b3f01972e681631a69fba14eef41505a1538ad6
greatestCat <cyan3441@naver.com>
2025-12-10 17:27:27 +0900

    STS 개발 환경 세팅 - [25.12.09_태훈]
    1. war 파일 배포 기능
    2. swagger 라이브러리 추가
    3. 기본 라이브러리 추가
    spring web
    spring devtools
    websocket
    mybatis framwork
    lombok
    spring sercurity
    oracle driver

---
+++ retrogram-backend/src/main/java/com/kh/retrogram/RetrogramBackendApplication.java
@@ -0,0 +1,17 @@
+package com.kh.retrogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RetrogramBackendApplication   {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrogramBackendApplication.class, args);
+	}
+
+	// war로 배포하겠다고 코드 작성
+	// 추후 삽입
+}
---
+++ retrogram-backend/src/test/java/com/kh/retrogram/RetrogramBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.retrogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrogramBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
