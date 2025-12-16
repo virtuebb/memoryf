2cb5251f9ccc85896d73c02714e4ee4456c21fbd
정민 <scence31@gmail.com>
2025-12-16 16:09:43 +0900

    Update SecurityConfig.java

--- Memoryf-backend/src/main/java/com/kh/memoryf/config/SecurityConfig.java
+++ Memoryf-backend/src/main/java/com/kh/memoryf/config/SecurityConfig.java
@@ -3,10 +3,7 @@ package com.kh.memoryf.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -17,26 +14,60 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig {
 
+	// 암호화
     @Bean
     public static BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+    
+    // 로그인 CORS 설정 ( cors 활성화 + OPTIONS 허용)
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+    	
+    		CorsConfiguration config = new CorsConfiguration();
+    		config.addAllowedOrigin("http://localhost:5173");
+    		config.addAllowedHeader("*");
+    		config.addAllowedMethod("*");
+    		config.setAllowCredentials(true);
+    		
+    		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    		source.registerCorsConfiguration("/**", config);
+    		
+    		return source;
+    }
+    
+    // Spring Security 보안 규칙과 필터 체인 설정
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                // 사진파일 허용하기
-                .requestMatchers("/images/**").permitAll()
-                .requestMatchers("/resources/**").permitAll()
-                .requestMatchers("/css/**", "/js/**").permitAll()
-                // 일단 전체 허용했음
-                .anyRequest().permitAll()
-            )
-            // 로그인폼 삭제
-            .formLogin(form -> form.disable());
-
-        return http.build();
+    	
+    		http.cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 적용 - 리액트 요청 허용
+    			.csrf(csrf -> csrf.disable()) // CSRF 방어가 필요 없음 - 쿠키 기반 세션 안 씀, JWT 방식이라서
+    			.sessionManagement(session -> session.sessionCreationPolicy(
+    					org.springframework.security.config.http.SessionCreationPolicy.STATELESS)) // JWT 인증방식임 - 세션 아님
+    			.authorizeHttpRequests(auth -> auth.requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll() // 프리플라이트(OPTIONS) 요청 모두 허용
+    					.requestMatchers("/images/**", "/resources/**", "/css/**", "/js/**").permitAll() // 해당 정적 리소스 모두 허용
+    					.requestMatchers("/login/**").permitAll() // 로그인 요청 허용 - @RequestMapping("login") 관련
+    					.anyRequest().authenticated() // 나머지는 JWT 인증 필요함
+    				)
+    				.formLogin(form -> form.disable()); // 스프링 방식의 로그인 막기
+    		
+    		return http.build();
+    	
     }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
